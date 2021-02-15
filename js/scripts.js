23ab40449bb2e7aed1f913c590754bcbcc55ba08
Reiko1337 <kurets11@gmail.com>
2021-02-16 00:01:05 +0300

    Version 1.0

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+document.querySelector('.header__burger').addEventListener('click', function(){
+    this.classList.toggle('active')
+    document.getElementsByClassName('menu')[0].classList.toggle('active')
+    document.body.classList.toggle('lock')
+})
+
+const spoiler = document.querySelectorAll('.footer__item-title')
+
+ for (let i = 0; i < spoiler.length; i++) {
+    spoiler[i].addEventListener("click", function() {
+        this.nextElementSibling.classList.toggle('spoiler-active')
+     });
+ }
+
+
+
+/* Индекс слайда по умолчанию */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slider__item");
+    let dots = document.getElementsByClassName("slider__dots-item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
